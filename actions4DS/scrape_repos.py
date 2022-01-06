341aa795a7583ee478c5d89208557e6b002fb3f1
Luigi Quaranta <quarantalgu@gmail.com>
2022-01-06 13:36:56 +0100

    Use rich progress bar in DataScienceScraper

--- actions4DS/scrape_repos.py
+++ actions4DS/scrape_repos.py
@@ -3,10 +3,12 @@ import logging
 import re
 import time
 from pathlib import Path
+from typing import Text
 
 from github import Github
 from github.GithubException import UnknownObjectException
 from models import GitHubSlug
+from rich.progress import BarColumn, Progress, TimeRemainingColumn
 from ruamel.yaml import YAML
 from tqdm import tqdm
 
@@ -18,23 +20,6 @@ class GitHubScraper:
 
         # Set up the GitHub instance
         self.github = Github(token_list[0])
-
-        # Set up the yaml parser
-        self.yaml_parser = YAML()
-
-        # Set up the data directory
-        if not data_dir.exists:
-            raise ValueError("The specified data directory does not exist.")
-        if not data_dir.is_dir:
-            raise ValueError("The specified data directory is not a folder.")
-        else:
-            self.data_dir = data_dir
-
-    def _update_pbar_description(self, pbar):
-        """Update the total number of workflows displayed in the progress bar."""
-        pbar.set_description(
-            f"Total workflows found = {self.scraping_stats['total_number_of_workflows']}"
-        )
 
     def _check_rate_limit(self):
         """Check the rate limit of the Github API."""
@@ -88,25 +73,27 @@ class DataScienceScraper(GitHubScraper):
 
     def __init__(self, token_list: list[str], data_dir: Path) -> None:
         super().__init__(token_list, data_dir)
+
+        # Initialize scraping stats
         self.scraping_stats = {
-            "repos_with_at_least_one_matching_keyword": 0,
+            "repos_not_found": 0,
+            "repos_with_keywords_in_topics": 0,
+            "repos_with_keywords_in_description": 0,
+            "selected_repos": 0,
         }
 
     def __str__(self) -> str:
         summary = "\nSCRAPING SUMMARY\n"
-        summary += "  - Data science repositories with at least one matching keyword = "
-        summary += (
-            f"{self.scraping_stats['repos_with_at_least_one_matching_keyword'] };\n"
-        )
+        summary += "  - Repositories with at least one matching keyword (selected) = "
+        summary += f"{self.scraping_stats['selected_repos'] };\n"
+        summary += "  - Repositories with matching keyword(s) in topics = "
+        summary += f"{self.scraping_stats['repos_with_keywords_in_topics'] };\n"
+        summary += "  - Repositories with matching keyword(s) in description = "
+        summary += f"{self.scraping_stats['repos_with_keywords_in_description'] };\n"
+        summary += "  - Repositories not found = "
+        summary += f"{self.scraping_stats['repos_not_found'] };\n"
 
         return summary
-
-    def _update_pbar_description(self, pbar):
-        """Update the total DS repos displayed in the progress bar."""
-        pbar.set_description(
-            "Total DS repos found = "
-            f"{self.scraping_stats['repos_with_at_least_one_matching_keyword']}"
-        )
 
     def scrape_repos(self, slugs: list[GitHubSlug]) -> list[GitHubSlug]:
         """Scrape GitHub repos for data science.
@@ -119,33 +106,69 @@ class DataScienceScraper(GitHubScraper):
             list[GitHubSlug]: list of GitHub slugs with data science keywords
         """
         ds_slugs = []
-        pbar = tqdm(slugs)
-        for slug in pbar:
-            try:
+
+        with Progress(
+            "[progress.description]{task.description}",
+            BarColumn(),
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            "[progress.completed_ratio]{task.completed}/{task.total}",
+            "[progress.selected_repos][bright_black]Selected: {task.fields[selected]}",
+            TimeRemainingColumn(),
+        ) as progress:
+
+            task = progress.add_task(
+                "Filtering GitHub Repos...", total=len(slugs), selected=0
+            )
+
+            for slug in slugs:
+
+                # Check GitHub API rate limit: wait if needed
                 self._check_rate_limit()
-                repo = self.github.get_repo(str(slug))
+
                 try:
-                    topics = " ".join(repo.get_topics())
+                    repo = self.github.get_repo(str(slug))
+
+                    try:
+                        topics = " ".join(repo.get_topics())
+                    except UnknownObjectException:
+                        topics = ""
+                    description = repo.description or ""
+
+                    for keyword in self.KEYWORDS:
+                        keywords_in_topics = re.search(keyword, topics, re.IGNORECASE)
+                        keywords_in_description = re.search(
+                            keyword, description, re.IGNORECASE
+                        )
+                        if keywords_in_topics or keywords_in_description:
+                            ds_slugs.append(slug)
+                            progress.console.log(
+                                f':thumbs_up: Data science repo found: "{slug}".',
+                            )
+
+                            # Update scraping stats
+                            if keywords_in_topics:
+                                self.scraping_stats[
+                                    "repos_with_keywords_in_topics"
+                                ] += 1
+                            if keywords_in_description:
+                                self.scraping_stats[
+                                    "repos_with_keywords_in_description"
+                                ] += 1
+                            self.scraping_stats["selected_repos"] += 1
+
                 except UnknownObjectException:
-                    topics = ""
-                description = repo.description or ""
+                    self.scraping_stats["repos_not_found"] += 1
+                    progress.console.log(
+                        f':cross_mark: Repository not found: "{slug}".',
+                    )
+                finally:
+                    progress.update(
+                        task,
+                        advance=1,
+                        selected=self.scraping_stats["selected_repos"],
+                    )
 
-                for keyword in self.KEYWORDS:
-                    if re.search(keyword, topics, re.IGNORECASE) or re.search(
-                        keyword, description, re.IGNORECASE
-                    ):
-                        ds_slugs.append(slug)
-                        # Update scraping stats
-                        self.scraping_stats[
-                            "repos_with_at_least_one_matching_keyword"
-                        ] += 1
-
-            except UnknownObjectException as uoe:
-                logging.error(f"UnknownObjectException: {uoe}")
-            finally:
-                self._update_pbar_description(pbar)
-
-        print(self)
+        logging.info("[Filtering slugs from RepoReaper] " + str(self))
         return ds_slugs
 
 
@@ -157,11 +180,23 @@ class WorkflowScraper(GitHubScraper):
 
     def __init__(self, token_list: list[str], data_dir: Path) -> None:
         super().__init__(token_list, data_dir)
+
         # Initialize scraping stats
         self.scraping_stats = {
             "repos_with_at_least_one_workflow": 0,
             "total_number_of_workflows": 0,
         }
+
+        # Set up the yaml parser
+        self.yaml_parser = YAML()
+
+        # Set up the data directory
+        if not data_dir.exists:
+            raise ValueError("The specified data directory does not exist.")
+        if not data_dir.is_dir:
+            raise ValueError("The specified data directory is not a folder.")
+        else:
+            self.data_dir = data_dir
 
     def _update_pbar_description(self, pbar):
         """Update the total number of workflows displayed in the progress bar."""
